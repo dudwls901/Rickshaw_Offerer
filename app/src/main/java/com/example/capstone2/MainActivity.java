c88af98cb795cf3c133ff50f0197ebf3d6942705
Won-dy <junhfireace@gmail.com>
2020-06-04 23:02:38 +0900

    동연테스트2

--- app/src/main/java/com/example/capstone2/MainActivity.java
+++ app/src/main/java/com/example/capstone2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 동연 테스트
+        // 동연 테스트 2
     }
 }
