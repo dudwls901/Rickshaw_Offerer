b1fc7af7ea6063ac6ac38de73ab3555b7ce5b13d
Won-dy <junhfireace@gmail.com>
2020-06-17 01:44:48 +0900

    스플래시 / 비번변경 추가

---
+++ app/src/main/java/kr/co/ilg/activity/login/SplashActivity.java
@@ -0,0 +1,19 @@
+package kr.co.ilg.activity.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
--- app/src/main/java/kr/co/ilg/activity/mypage/PasswordChangeActivity.java
+++ app/src/main/java/kr/co/ilg/activity/mypage/PasswordChangeActivity.java
@@ -1,7 +1,10 @@
 package kr.co.ilg.activity.mypage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
@@ -9,9 +12,21 @@ import com.example.capstone2.R;
 
 public class PasswordChangeActivity extends Activity {
 
+    Button changeBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.password_change);
+
+        changeBtn = findViewById(R.id.changeBtn);
+
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PasswordChangeActivity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
