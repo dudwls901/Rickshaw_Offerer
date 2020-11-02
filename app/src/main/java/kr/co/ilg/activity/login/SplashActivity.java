36042e4e8af84b2d688cdbd9c2fcaa91b1c73466
정선우 <miney8332@naver.com>
2020-11-02 17:44:01 +0900

    로그인 완료 코틀린 추가

--- app/src/main/java/kr/co/ilg/activity/login/SplashActivity.java
+++ app/src/main/java/kr/co/ilg/activity/login/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        Intent intent = new Intent(SplashActivity.this, com.example.capstone2.MainActivity.class);
         startActivity(intent);
 
         finish();
